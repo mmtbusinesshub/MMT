496d69fc7f85958c60a56e692b974df704618fa7
mmtbusinesshub <mmtbusinesshub@gmail.com>
2025-11-07 18:52:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 const defaultConfig = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "FAUzFTza#iN8AjiiYAqD7_8b-fIX8ike3PvGjlc_17cNMgwjm_hs", // Put your session id here
   ALIVE_IMG: "https://github.com/mmtbusinesshub/MMT/blob/main/images/download.png?raw=true",
   ALIVE_MSG: "",
   BOT_OWNER: "94774915917", // Replace your bot owner number here with 94(country code)
